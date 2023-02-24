65aae08621d0ae2d7c2a6e039e5ce04b6f7f7e70
subrabala <subrabala_dash@srmap.edu.in>
2023-02-24 23:35:31 +0530

    first

--- script.js
+++ script.js
@@ -2,10 +2,41 @@ document.addEventListener('DOMContentLoaded', function () {
     setTimeout(function () {
         document.getElementById("text-wrapper").style.display = 'none';
     }, 3900);
-    setTimeout(function(){
-        const ntl_text = document.getElementById("ntl_text");
-        ntl_text.style.webkitTextFillColor = 'transparent';
-        ntl_text.style.webkitBackgroundClip = 'text';
-    },4100);
+    // setTimeout(function(){
+    //     const ntl_text = document.getElementById("ntl_text");
+    //     const overlay = document.getElementById("overlay");
+    //     ntl_text.style.backgroundImage= 'assets/people.png';
+    //     ntl_text.style.webkitTextFillColor = 'transparent';
+    //     ntl_text.style.webkitBackgroundClip = 'text';
+        
+    //     // document.getElementById("overlay").style.backgroundColor = 'transparent';
+    // },4100);
+    // setTimeout(function () {
+        
+    //     const ntl_text = document.getElementById("ntl_text");
+    //     ntl_text.style.color = 'transparent';
+    //     ntl_text.style.backgroundImage='url("assets/people.png")';
+    //     ntl_text.style.webkitTextFillColor = 'transparent';
+    //     ntl_text.style.webkitBackgroundClip = 'text';
+    //     // ntl_text.style.overflow = 'hidden';
+    //     // document.getElementById("overlay").style.backgroundColor = 'trans';
+    //     let i = 0;
+    //     document.getElementById("overlay").addEventListener('wheel', function(e){
+    //         if(e.wheelDelta > 0){
+    //             i++;
+    //         } else if(i>1){
+    //             i--;
+    //         }
+    //         nxt = document.getElementById("nxt");
+    //         nxt.style.color = 'transparent';
+    //         nxt.style.backgroundImage='url("assets/people.png")';            
+    //         nxt.style.webkitTextFillColor = 'transparent';
+    //         nxt.style.webkitBackgroundClip = 'text';
+    //         // nxt.style.transform = `translate(${i*1.1}px, ${i*100}px) scale(${i*1.1})`;
+    //     });
+    // }, 4100);
   });
+
+
+
 
